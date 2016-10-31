a7e683bfba3e405214e694b4b9f1835504fc8aed
evykassirer <evy.kassirer@gmail.com>
2016-10-31 12:04:10 -0400

    take out a TODO (moved to asana)

--- test/expression/step-solver/cancelLikeTerms.test.js
+++ test/expression/step-solver/cancelLikeTerms.test.js
@@ -21,7 +21,7 @@ describe('cancel like terms', function () {
     ['x^3/x^2', 'x^(3-(2))'], // parens will be removed at end of step
     ['(x^3*y)/x^2', '(x^(3-(2))*y)'],
     ['-(7+x)^8/(7+x)^2', '-(7+x)^(8-(2))'],
-    ['(2x^2 * 5) / (2x^2)', '5'], // these parens have to stay around 2x^2 to be parsed correctly. TODO: make sure I'm not removing them in remove uncessary parens
+    ['(2x^2 * 5) / (2x^2)', '5'], // these parens have to stay around 2x^2 to be parsed correctly.
     ['(x^2 * y) / x', '(x^(2 - (1)) * y)'],
     ['2x^2 / (2x^2 * 5)', '1 / 5'],
     ['x / (x^2*y)', 'x^(1-(2)) / y'],
