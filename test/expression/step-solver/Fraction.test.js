25c73f47fc1ea9db7ee18a7cbec00b7912087f69
evykassirer <evy.kassirer@gmail.com>
2016-11-07 12:57:43 -0500

    add test

--- test/expression/step-solver/Fraction.test.js
+++ test/expression/step-solver/Fraction.test.js
@@ -55,6 +55,7 @@ describe('multiplyFractions', function () {
   const tests = [
     ['3 * 1/5 * 5/9', '(3*1*5)/(5*9)'],
     ['3/7 * 10/11', '(3*10)/(7*11)'],
+    ['2 * 5/x', '(2*5)/x']
   ];
   tests.forEach(t => testMultiplyFractions(t[0], t[1]));
 });
