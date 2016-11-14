c1cfb7080f76b7b2a20956054d46a2c0ddac5c6e
evykassirer <evy.kassirer@gmail.com>
2016-11-14 16:43:19 -0500

    take out latex

--- test/expression/step-solver/print.test.js
+++ test/expression/step-solver/print.test.js
@@ -5,11 +5,9 @@ const math = require('../../../index');
 const print = require('../../../lib/expression/step-solver/print.js');
 const flatten = require('../../../lib/expression/step-solver/flattenOperands.js');
 
-function testPrint(exprStr, outputStr, latex=false) {
+function testPrint(exprStr, outputStr) {
   it(exprStr + ' -> ' + outputStr, function () {
-    assert.deepEqual(
-      print(math.parse(exprStr), latex),
-      outputStr);
+    assert.deepEqual(print(math.parse(exprStr)), outputStr);
   });
 }
 
@@ -21,14 +19,4 @@ describe('print asciimath', function () {
     ['-2/3', '-2/3'],
   ];
   tests.forEach(t => testPrint(t[0], t[1]));
-});
-
-describe('print latex', function () {
-  const tests = [
-    ['2+3+4', '2 + 3 + 4'],
-    ['2 + (4 - x) + - 4', '2 + \\left(4 - x\\right) - 4'],
-    ['2 * 3', '2 \\cdot 3'],
-    ['2/3 x^2', '\\frac{2}{3} {x}^{2}'],
-  ];
-  tests.forEach(t => testPrint(t[0], t[1], true));
 });
