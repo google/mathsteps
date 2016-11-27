806642523f77e491858879038675e4a91422015b
Ahmed Elnaiem <aelnaiem@gmail.com>
2016-11-27 11:26:59 -0300

    adding a simple function to get prime factors for a number

---
+++ test/Factors.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const assert = require('assert');
+const Factors = require('../lib/Factors');
+
+function testPrimeFactors(input, output) {
+  it(input + ' -> ' + output, function () {
+    assert.deepEqual(
+      Factors.getPrimeFactors(input),
+      output);
+  });
+}
+
+describe('prime factors', function() {
+  const tests = [
+    [1, [1]],
+    [-1, [-1, 1]],
+    [-2, [-1, 2]],
+    [5, [5]],
+    [12, [2, 2, 3]],
+    [15, [3, 5]],
+    [36, [2, 2, 3, 3]],
+    [49, [7, 7]],
+    [1260, [2, 2, 3, 3, 5, 7]],
+    [13195, [5, 7, 13, 29]],
+    [1234567891, [1234567891]]
+  ];
+  tests.forEach(t => testPrimeFactors(t[0], t[1]));
+});
