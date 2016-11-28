c7be01ea51e4ad01b109aa19c0741981a01b8c9b
evykassirer <evy.kassirer@gmail.com>
2016-11-28 12:56:40 -0500

    test file

---
+++ test/evaluateConstantSum.test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const assert = require('assert');
+const math = require('mathjs');
+
+const evaluateConstantSum = require('../lib/evaluateConstantSum');
+const flatten = require('../lib/flattenOperands');
+const print = require('../lib/print');
+
+function testEvaluateConstantSum(exprString, outputList) {
+  const lastString = outputList[outputList.length - 1];
+  it(exprString + ' -> ' + lastString, function () {
+    const status = evaluateConstantSum(flatten(math.parse(exprString)));
+    const subSteps = status.subSteps;
+
+    assert.deepEqual(subSteps.length, outputList.length);
+    subSteps.forEach((step, i) => {
+      assert.deepEqual(
+        print(step.newNode),
+        outputList[i]);
+    });
+
+    assert.deepEqual(
+      print(status.newNode),
+      lastString);
+  });
+}
+
+describe('evaluateConstantSum', function () {
+  const tests = [
+    ['4/10 + 3/5',
+      ['4/10 + (3 * 2) / (5 * 2)',
+        '4/10 + (3 * 2) / 10',
+        '4/10 + 6/10',
+        '(4 + 6) / 10',
+        '10/10',
+        '1']
+    ],
+    ['4/5 + 3/5 + 2',
+      ['2 + (4/5 + 3/5)',
+        '2 + 7/5',
+        '17/5']
+    ],
+    ['9 + 4/5 + 1/5 + 2',
+      ['(9 + 2) + (4/5 + 1/5)',
+        '11 + (4/5 + 1/5)',
+        '11 + 1',
+        '12']
+    ],
+  ];
+  tests.forEach(t => testEvaluateConstantSum(t[0], t[1]));
+});
