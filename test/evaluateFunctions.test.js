fb6f26f30ca3468c5e902ef8f767676b0061846a
Ahmed Elnaiem <aelnaiem@gmail.com>
2016-12-05 14:03:44 -0300

    better comments and tests for roots steps

--- test/evaluateFunctions.test.js
+++ test/evaluateFunctions.test.js
@@ -7,17 +7,27 @@ const flatten = require('../lib/flattenOperands');
 const print = require('./../lib/print');
 const evaluateFunctions = require('../lib/evaluateFunctions');
 
-function testEvaluateFunctions(exprStr, outputStr) {
-  it(exprStr + ' -> ' + outputStr, function () {
+function testEvaluateFunctions(exprString, outputStr) {
+  it(exprString + ' -> ' + outputStr, function () {
     assert.deepEqual(
-      print(evaluateFunctions(flatten(math.parse(exprStr))).newNode),
+      print(evaluateFunctions(flatten(math.parse(exprString))).newNode),
       outputStr);
   });
 }
-describe('nthRoot', function () {
+
+describe('abs', function () {
+  const tests = [
+    ['abs(4)', '4'],
+    ['abs(-5)', '5'],
+  ];
+  tests.forEach(t => testEvaluateFunctions(t[0], t[1]));
+});
+
+describe('simplify nthRoot', function () {
   const tests = [
     ['nthRoot(4)', '2'],
     ['nthRoot(8, 3)', '2'],
+    ['nthRoot(5 * 7)', 'nthRoot(5 * 7)'],
     ['nthRoot(4, 3)', 'nthRoot(4, 3)'],
     ['nthRoot(12)', '2 * nthRoot(3, 2)'],
     ['nthRoot(36)', '6'],
@@ -37,10 +47,57 @@ describe('nthRoot', function () {
   ];
   tests.forEach(t => testEvaluateFunctions(t[0], t[1]));
 });
-describe('abs', function () {
+
+function testEvaluateSteps(exprString, outputList) {
+  const lastString = outputList[outputList.length - 1];
+  it(exprString + ' -> ' + lastString, function () {
+    const status = evaluateFunctions(flatten(math.parse(exprString)));
+    const subSteps = status.subSteps;
+
+    assert.deepEqual(subSteps.length, outputList.length);
+    subSteps.forEach((step, i) => {
+      assert.deepEqual(
+        print(step.newNode),
+        outputList[i]);
+    });
+
+    assert.deepEqual(
+      print(status.newNode),
+      lastString);
+  });
+}
+
+describe('nthRoot steps', function () {
   const tests = [
-    ['abs(4)', '4'],
-    ['abs(-5)', '5'],
+    ['nthRoot(12)',
+      ['nthRoot(2 * 2 * 3)',
+        'nthRoot((2 * 2) * 3)',
+        'nthRoot(2 ^ 2 * 3)',
+        'nthRoot(2 ^ 2, 2) * nthRoot(3, 2)',
+        '2 * nthRoot(3, 2)']
+    ],
+    ['nthRoot(72)',
+      ['nthRoot(2 * 2 * 2 * 3 * 3)',
+        'nthRoot((2 * 2) * 2 * (3 * 3))',
+        'nthRoot(2 ^ 2 * 2 * 3 ^ 2)',
+        'nthRoot(2 ^ 2, 2) * nthRoot(2, 2) * nthRoot(3 ^ 2, 2)',
+        '2 * nthRoot(2, 2) * 3',
+        '2 * 3 * nthRoot(2, 2)']
+    ],
+    ['nthRoot(36*x)',
+      ['nthRoot(2 * 2 * 3 * 3 * x)',
+        'nthRoot((2 * 2) * (3 * 3) * x)',
+        'nthRoot(2 ^ 2 * 3 ^ 2 * x)',
+        'nthRoot(2 ^ 2, 2) * nthRoot(3 ^ 2, 2) * nthRoot(x, 2)',
+        '2 * 3 * nthRoot(x, 2)']
+    ],
+    ['nthRoot(2 * 18 * x ^ 2, 2)',
+      ['nthRoot(2 * 2 * 3 * 3 * x ^ 2, 2)',
+        'nthRoot((2 * 2) * (3 * 3) * x ^ 2, 2)',
+        'nthRoot(2 ^ 2 * 3 ^ 2 * x ^ 2, 2)',
+        'nthRoot(2 ^ 2, 2) * nthRoot(3 ^ 2, 2) * nthRoot(x ^ 2, 2)',
+        '2 * 3 * x']
+    ]
   ];
-  tests.forEach(t => testEvaluateFunctions(t[0], t[1]));
+  tests.forEach(t => testEvaluateSteps(t[0], t[1]));
 });
