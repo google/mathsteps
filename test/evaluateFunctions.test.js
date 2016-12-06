8111449a0612e260847660fbed11b5e64033f682
Ahmed Elnaiem <aelnaiem@gmail.com>
2016-12-06 12:35:47 -0300

    subSteps -> substeps

--- test/evaluateFunctions.test.js
+++ test/evaluateFunctions.test.js
@@ -52,10 +52,10 @@ function testEvaluateSteps(exprString, outputList) {
   const lastString = outputList[outputList.length - 1];
   it(exprString + ' -> ' + lastString, function () {
     const status = evaluateFunctions(flatten(math.parse(exprString)));
-    const subSteps = status.subSteps;
+    const substeps = status.substeps;
 
-    assert.deepEqual(subSteps.length, outputList.length);
-    subSteps.forEach((step, i) => {
+    assert.deepEqual(substeps.length, outputList.length);
+    substeps.forEach((step, i) => {
       assert.deepEqual(
         print(step.newNode),
         outputList[i]);
