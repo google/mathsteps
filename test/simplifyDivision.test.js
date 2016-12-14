3f8efc62f132a6be5f35e35d673b9f5ae07d423c
evykassirer <evy.kassirer@gmail.com>
2016-12-14 16:13:35 -0500

    move multiplyByInverse to simplifyDivision

--- test/simplifyDivision.test.js
+++ test/simplifyDivision.test.js
@@ -22,6 +22,9 @@ describe('simplifyDivision', function () {
     ['-6/x/5', '-6 / (x * 5)'],
     ['(2+2)/x/6/(y-z)','(2 + 2) / (x * 6 * (y - z))'],
     ['2/x', '2 / x'],
+    ['x/(2/3)', 'x * 3/2'],
+    ['x / (y/(z+a))', 'x * (z + a) / y'],
+    ['x/((2+z)/(3/y))', 'x * (3 / y) / (2 + z)'],
   ];
   tests.forEach(t => testSimplifyDivision(t[0], t[1]));
 });
