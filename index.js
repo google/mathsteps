095d47205b89cd969b53617cae6f972d8f66343c
evykassirer <evy.kassirer@gmail.com>
2016-12-26 11:29:02 -0500

    remove old module exports

--- index.js
+++ index.js
@@ -5,13 +5,7 @@ const solveEquationString = require('./lib/solveEquation/solveEquationString');
 const MathChangeTypes = require('./lib/MathChangeTypes');
 
 module.exports = {
-  // TODO: after deploying this and then the update to Athena.
-  // remove expressionStepper
-  expressionStepper: simplifyExpressionNode,
   simplifyExpressionString: simplifyExpressionString,
-  // TODO: after deploying this and then the update to Athena,
-  // remove equationStepper
-  equationStepper: solveEquation,
   solveEquationString: solveEquationString,
   MathChangeTypes: MathChangeTypes,
 };
