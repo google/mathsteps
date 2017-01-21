2b0a6c4590fc83a180df4b809ad20b989047e864
Richard Lee <rblee88@gmail.com>
2017-01-22 01:08:19 +1100

    Implement EquationOperations.removeSymbolFromDenominator

--- test/solveEquation/solveEquation.test.js
+++ test/solveEquation/solveEquation.test.js
@@ -89,6 +89,11 @@ describe('solveEquation for =', function () {
     ['x^2 = 0', 'x = [0, 0]'],
     ['x^(2) = 0', 'x = [0, 0]'],
     ['(x+2)^2 -x^2 = 4(x+1)', '4 = 4'],
+    ['2/x = 1', 'x = 2'],
+    ['2/(4x) = 1', 'x = 1/2'],
+    ['2/(8 - 4x) = 1/2', 'x = 1'],
+    ['2/(1 + 1 + 4x) = 1/3', 'x = 1'],
+    ['(3 + x) / (x^2 + 3) = 1', 'x = [0, 1]'],
     // TODO: fix these cases, fail because lack of factoring support, for complex #s,
     // for taking the sqrt of both sides, etc
     // ['(x + y) (y + 2) = 0', 'y = -y'],
