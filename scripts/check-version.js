e9c98d4f7e45946d230d9b2cc08a232dec63ff06
Tommasone, Mattia(AWF) <mtommasone@ebay.com>
2017-01-26 11:41:10 +0100

    Lint fixes and script in the correct place in package.json

--- scripts/check-version.js
+++ scripts/check-version.js
@@ -1,10 +1,18 @@
+/*global process*/
+'use strict';
+
 const semver = require('semver');
 const engines = require('../package').engines;
 
 const version = engines.node;
 if (!semver.satisfies(process.version, version)) {
-  console.log(`Required node version ${version} not satisfied with current version ${process.version}.`);
+  // eslint-disable-next-line
+  console.log(`Required node version ${version} not satisfied with current ` + 
+    `version ${process.version}.`);
   process.exit(1);
-} else {
-  console.log(`Current node version ${process.version} satisfies version requirement ${version}`);
+}
+else {
+  // eslint-disable-next-line
+  console.log(`Current node version ${process.version} satisfies version ` + 
+   `requirement ${version}`);
 }
