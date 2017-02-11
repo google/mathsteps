d45a35c9c6f10231a913703d436d842b3b381958
Nitin <tulswani19@gmail.com>
2017-02-12 01:20:40 +0530

    some corrections

--- sample.js
+++
@@ -1,4 +0,0 @@
-const collector = require('./lib/simplifyExpression/collectAndCombineSearch/LikeTermCollector');
-const math = require('mathjs');
-
-console.log(collector.collectLikeTerms(math.parse('x^2 + x + 3 + x^2 + 2')));
