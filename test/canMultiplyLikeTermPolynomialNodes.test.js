1427e88a3ea40e288b245ff6ef831be083c1e12e
Anthony <aliang8@stuy.edu>
2017-05-13 22:51:52 -0400

    revert

--- test/canMultiplyLikeTermPolynomialNodes.test.js
+++ test/canMultiplyLikeTermPolynomialNodes.test.js
@@ -10,8 +10,7 @@ describe('can multiply like term polynomials', () => {
   const tests = [
     ['x^2 * x * x', true],
     ['x^2 * 3x * x', false],
-    ['y * y^3', true],
-    ['x^3 * x^2', true]
+    ['y * y^3', true]
   ];
   tests.forEach(t => testCanBeMultiplied(t[0], t[1]));
 });
