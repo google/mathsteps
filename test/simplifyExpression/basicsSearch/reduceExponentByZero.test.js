2e8dd81522cc8d27f542df138b6bec2f8122658b
Anthony <aliang8@stuy.edu>
2017-06-14 00:38:17 -0400

    removing test files, will move tests to math-rules

--- test/simplifyExpression/basicsSearch/reduceExponentByZero.test.js
+++
@@ -1,6 +0,0 @@
-import {basics} from '../../../lib/simplifyExpression/search.js';
-import testSimplify from './testSimplify.js';
-
-describe('reduceExponentByZero', function() {
-  testSimplify('(x+3)^0', '1', basics);
-});
--- test/simplifyExpression/basicsSearch/removeExponentByOne.test.js
+++
@@ -1,6 +0,0 @@
-import {basics} from '../../../lib/simplifyExpression/search.js';
-import testSimplify from './testSimplify.js';
-
-describe('removeExponentByOne', function() {
-  testSimplify('x^1', 'x', basics);
-});
