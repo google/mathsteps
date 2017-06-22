dd7e55eaf3682f29f41d593514d7c2302c73bf7b
Anthony <aliang8@stuy.edu>
2017-06-22 16:08:43 -0400

    why doesn't it tell me all the issues at once?

--- test/simplifyExpression/basicsSearch/removeDivisionByOne.test.js
+++ test/simplifyExpression/basicsSearch/removeDivisionByOne.test.js
@@ -3,10 +3,10 @@ const removeDivisionByOne = require('../../../lib/simplifyExpression/basicsSearc
 const testSimplify = require('./testSimplify');
 
 describe('removeDivisionByOne', function() {
-    const tests = [
-        ['x/1', 'x'],
-        ['x/-1', '-x'],
-        ['-x/-1', 'x']
-    ];
-    tests.forEach(t => testSimplify(t[0], t[1], removeDivisionByOne));
+  const tests = [
+    ['x/1', 'x'],
+    ['x/-1', '-x'],
+    ['-x/-1', 'x']
+  ];
+  tests.forEach(t => testSimplify(t[0], t[1], removeDivisionByOne));
 });
