f25ad34438ce0c6a28559e3079a9d163da1b8323
Anthony <aliang8@stuy.edu>
2017-06-22 16:02:54 -0400

    more linter issues

--- test/simplifyExpression/basicsSearch/removeMultiplicationByOne.test.js
+++ test/simplifyExpression/basicsSearch/removeMultiplicationByOne.test.js
@@ -4,10 +4,10 @@ const testSimplify = require('./testSimplify');
 
 describe('removeMultiplicationByOne', function() {
   const tests = [
-      ['x*1', 'x'],
-      ['1x', 'x'],
-      ['1*z^2', 'z^2'],
-      ['2*1*z^2', '2 * z^2'],
+    ['x*1', 'x'],
+    ['1x', 'x'],
+    ['1*z^2', 'z^2'],
+    ['2*1*z^2', '2 * z^2'],
   ];
   tests.forEach(t => testSimplify(t[0], t[1], removeMultiplicationByOne));
 });
