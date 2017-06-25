4976820d3c069568876525cc2b559072f157f7b1
Anthony <aliang8@stuy.edu>
2017-06-25 16:06:55 -0400

    linter strikes again

--- test/simplifyExpression/basicsSearch/basicTest.js
+++ test/simplifyExpression/basicsSearch/basicTest.js
@@ -10,9 +10,9 @@ describe('basic rules', function() {
       ['--1','1'],
       ['--x','x'],
       ['--(x + 1)', 'x + 1'],
-    ]
+    ];
     tests.forEach(t => testSimplify(t[0], t[1], basic_rules.negation));
-  })
+  });
 
   describe.skip('rearrange coefficient', function() {
     const tests = [
@@ -20,27 +20,27 @@ describe('basic rules', function() {
         ['yz * 3', '3 yz'],
         // TODO: handle this case better
         //['3x^2 * 5', '5 (3 x^2)']
-    ]
+    ];
     tests.forEach(t => testSimplify(t[0], t[1], basic_rules.REARRANGE_COEFF));
-  })
+  });
 
   describe('division by negative one', function() {
     const tests = [
       ['2 / -1','-2'],
       ['x / -1','-x'],
       ['(x + 1) / -1', '-(x + 1)'],
-    ]
+    ];
     tests.forEach(t => testSimplify(t[0], t[1], basic_rules.removeDivisionByOne));
-  })
+  });
 
   describe('division by one', function() {
     const tests = [
       ['2 / 1', '2'],
       ['x / 1', 'x'],
       ['(x + 1) / 1', 'x + 1'],
-    ]
+    ];
     tests.forEach(t => testSimplify(t[0], t[1], basic_rules.removeDivisionByOne));
-  })
+  });
 
   describe('multiply by zero', function() {
     const tests = [
@@ -53,27 +53,27 @@ describe('basic rules', function() {
       ['0 * x', '0'],
       ['0 x', '0'],
       ['0 * (x + 1)', '0'],
-    ]
+    ];
     tests.forEach(t => testSimplify(t[0], t[1], basic_rules.removeMultiplicationByZero));
-  })
+  });
 
   describe('reduce exponent by zero', function() {
     const tests = [
       ['2 ^ 0', '1'],
       ['x ^ 0', '1'],
       ['(x + 1) ^ 0', '1'],
-    ]
+    ];
     tests.forEach(t => testSimplify(t[0], t[1], basic_rules.removeExponentByZero));
-  })
+  });
 
   describe('reduce zero numerator', function() {
     const tests = [
       ['0 / 2', '0'],
       ['0 / x', '0'],
       ['0 / (x + 1)', '0'],
-    ]
+    ];
     tests.forEach(t => testSimplify(t[0], t[1], basic_rules.removeDivisionByZero));
-  })
+  });
 
 
   describe('remove adding zero', function() {
@@ -86,27 +86,27 @@ describe('basic rules', function() {
       ['0 + 2', '2'],
       ['0 + x', 'x'],
       ['0 + (x + 1)', 'x + 1'],
-    ]
+    ];
     tests.forEach(t => testSimplify(t[0], t[1], basic_rules.removeAdditionOfZero));
-  })
+  });
 
   describe('remove exponent by one', function() {
     const tests = [
       ['2 ^ 1', '2'],
       ['x ^ 1', 'x'],
       ['(x + 1) ^ 1', 'x + 1'],
-    ]
+    ];
     tests.forEach(t => testSimplify(t[0], t[1], basic_rules.removeExponentByOne));
-  })
+  });
 
   describe('remove exponent by base one', function() {
     const tests = [
         ['1 ^ 2', '1'],
         //['1 ^ x', '1'],
         //['1 ^ (x + 1)', '1'],
-    ]
+    ];
     tests.forEach(t => testSimplify(t[0], t[1], basic_rules.removeExponentBaseOne));
-  })
+  });
 
   describe('remove multiplying by negative one', function() {
     const tests = [
@@ -114,9 +114,9 @@ describe('basic rules', function() {
       ['x * -1', '-x'],
       ['(x + 1) * -1', '-(x + 1)'],
       ['2x * 2 * -1', '2 x * -2'],
-    ]
+    ];
     tests.forEach(t => testSimplify(t[0], t[1], basic_rules.removeMultiplicationByNegativeOne));
-  })
+  });
 
   describe('remove multiplying by one', function() {
     const tests = [
@@ -130,9 +130,9 @@ describe('basic rules', function() {
       ['1 * x', 'x'],
       ['1 x', 'x'],
       ['1 * (x + 1)', 'x + 1'],
-    ]
+    ];
     tests.forEach(t => testSimplify(t[0], t[1], basic_rules.removeMultiplicationByOne));
-  })
+  });
 });
 
 function testSimplify(exprStr, outputStr, simplifyOperation) {
