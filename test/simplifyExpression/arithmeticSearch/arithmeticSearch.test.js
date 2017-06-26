7629e3f66d2c29ff667dfe30c561af4f0b603c57
Anthony <aliang8@stuy.edu>
2017-06-25 22:57:24 -0400

    working on arithmetic rules

--- test/simplifyExpression/arithmeticSearch/arithmeticSearch.test.js
+++ test/simplifyExpression/arithmeticSearch/arithmeticSearch.test.js
@@ -6,11 +6,17 @@ function testArithmeticSearch(exprStr, outputStr) {
   TestUtil.testSimplification(arithmeticSearch, exprStr, outputStr);
 }
 
-describe.skip('evaluate arithmeticSearch', function () {
+describe('evaluate arithmeticSearch', function () {
   const tests = [
     ['2+2', '4'],
     ['2*3*5', '30'],
-    ['9/4', '9/4'], //  does not divide
+    ['9/4', '9 / 4'], //  does not divide
+    ['1 + 2', '3'],
+    ['1 + 2 + 3', '6'],
+    ['3 * 8', '24'],
+    ['-2^2', '-4'],
+    ['(-2)^2', '4'],
+    ['1 + 2 + y', '3 + y'],
   ];
   tests.forEach(t => testArithmeticSearch(t[0], t[1]));
 });
