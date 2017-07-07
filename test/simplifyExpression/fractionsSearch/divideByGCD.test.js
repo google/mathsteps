5d9d520f4aa623acaa81a044edbced1ac092f3ad
Lili Dworkin <lilidworkin@gmail.com>
2017-07-07 16:09:23 -0400

    adding tests

--- test/simplifyExpression/fractionsSearch/divideByGCD.test.js
+++ test/simplifyExpression/fractionsSearch/divideByGCD.test.js
@@ -2,8 +2,12 @@ const divideByGCD = require('../../../lib/simplifyExpression/fractionsSearch/div
 
 const TestUtil = require('../../TestUtil');
 
-function testdivideByGCD(exprStr, outputStr) {
+function testDivideByGCD(exprStr, outputStr) {
   TestUtil.testSimplification(divideByGCD, exprStr, outputStr);
+}
+
+function testDivideByGCDSubsteps(exprString, outputList, outputStr) {
+  TestUtil.testSubsteps(divideByGCD, exprString, outputList, outputStr);
 }
 
 describe('simplifyFraction', function() {
@@ -15,5 +19,19 @@ describe('simplifyFraction', function() {
     ['-3/-2', '3/2'],
     ['-1/-1', '1'],
   ];
-  tests.forEach(t => testdivideByGCD(t[0], t[1]));
+  tests.forEach(t => testDivideByGCD(t[0], t[1]));
+});
+
+describe('simplifyFraction', function() {
+  const tests = [
+    ['15/6',
+     ['15/3 / (6/3)'],
+     '5/2',
+    ],
+    ['24/40',
+     ['24/8 / (40/8)'],
+     '3/5',
+     ]
+  ];
+  tests.forEach(t => testDivideByGCDSubsteps(t[0], t[1], t[2]));
 });
