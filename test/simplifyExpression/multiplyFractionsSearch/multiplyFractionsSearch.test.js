4c316a631d43091ba774782cbcd6690e57d2235b
Lili Dworkin <lilidworkin@gmail.com>
2017-07-07 09:07:49 -0400

    adding tests

--- test/simplifyExpression/multiplyFractionsSearch/multiplyFractionsSearch.test.js
+++ test/simplifyExpression/multiplyFractionsSearch/multiplyFractionsSearch.test.js
@@ -10,7 +10,9 @@ describe('multiplyFractions', function () {
   const tests = [
     ['3 * 1/5 * 5/9', '(3 * 1 * 5) / (5 * 9)'],
     ['3/7 * 10/11', '(3 * 10) / (7 * 11)'],
-    ['2 * 5/x', '(2 * 5) / x']
+    ['2 * 5/x', '(2 * 5) / x'],
+    ['2 * (5/x)', '(2 * 5) / x'],
+    ['(5/x) * (2/x)', '(5 * 2) / (x * x)'], 
   ];
   tests.forEach(t => testMultiplyFractionsSearch(t[0], t[1]));
 });
