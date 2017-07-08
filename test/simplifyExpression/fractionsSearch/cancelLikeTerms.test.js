e03f763a192953fec1ba35ca956a34a41df511b8
Anthony Liang <aliangdw@gmail.com>
2017-07-08 14:55:11 -0400

    Add two more cases for cancelling terms (#182)

--- test/simplifyExpression/fractionsSearch/cancelLikeTerms.test.js
+++ test/simplifyExpression/fractionsSearch/cancelLikeTerms.test.js
@@ -12,7 +12,7 @@ describe('cancel like terms', function () {
     ['x^2/x^2', '1'],
     ['x^3/x^2', 'x^(3 - (2))'], // parens will be removed at end of step
     ['(x^3*y)/x^2', '(x^(3 - (2)) * y)'],
-    ['-(7+x)^8/(7+x)^2', '-((7 + x)^(8 - (2)))'],
+    ['-(7+x)^8/(7+x)^2', '-(7 + x)^(8 - (2))'],
     ['(2x^2 * 5) / (2x^2)', '5'], // these parens have to stay around 2x^2 to be parsed correctly.
     ['(x^2 * y) / x', '(x^(2 - (1)) * y)'],
     ['2x^2 / (2x^2 * 5)', '1/5'],
@@ -21,13 +21,10 @@ describe('cancel like terms', function () {
     ['(2x+5)^8 / (2x+5)^2', '(2x + 5)^(8 - (2))'],
     ['(4x^3) / (5x^2)', '(4x^(3 - (2))) / (5)'],
     ['-x / -x', '1'],
+    ['2/ (4x)', '1 / (2x)'],
+    ['2/ (4x^2)', '1 / (2x^2)'],
+    ['(35 * nthRoot (7)) / (5 * nthRoot(5))','(7 * nthRoot(7)) / nthRoot(5)'],
   ];
 
-  tests.forEach(t => {
-    const before = t[0];
-    const after = t[1];
-    it(before + ' -> ' + after, function () {
-      testCancelLikeTerms(before, after);
-    });
-  });
+  tests.forEach(t => testCancelLikeTerms(t[0], t[1]));
 });
