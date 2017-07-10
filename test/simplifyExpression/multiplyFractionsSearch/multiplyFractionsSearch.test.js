2d49b3e5bb3cbae2d00186fd1dd0477e3a33f3ee
Lili Dworkin <lilidworkin@gmail.com>
2017-07-10 15:00:45 -0400

    multiply fractions with symbol in denominator (#186)
    * has symbol in denominator function
    
    * moving over the symbol in denominator function
    
    * allow another case in multiply fractions
    
    * adding tests
    
    * updating comments
    
    * linter
    
    * linter
    
    * fix failing test

--- test/simplifyExpression/multiplyFractionsSearch/multiplyFractionsSearch.test.js
+++ test/simplifyExpression/multiplyFractionsSearch/multiplyFractionsSearch.test.js
@@ -13,9 +13,15 @@ describe('multiplyFractions', function () {
     ['2 * 5/x', '(2 * 5) / x'],
     ['2 * (5/x)', '(2 * 5) / x'],
     ['(5/x) * (2/x)', '(5 * 2) / (x * x)'],
+    ['(5/x) * x', '(5x) / x'],
+    ['2x * 9/x', '(2x * 9) / x'],
     ['-3/8 * 2/4', '(-3 * 2) / (8 * 4)'],
     ['(-1/2) * 4/5', '(-1 * 4) / (2 * 5)'],
     ['4 * (-1/x)', '(4 * -1) / x'],
+    ['x * 2y / x', '(x * 2y) / x'],
+    ['x/z * 1/2', '(x * 1) / (z * 2)'],
+    ['(6y / x) * 4x', '(6y * 4x) / x'],
+    ['2x * y / z * 10', '(2x * y * 10) / z'],
 
     // TODO: This should work too, but right now we don't handle
     // unary minus around parenthesis
