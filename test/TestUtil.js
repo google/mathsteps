38aeaf9cb17afe53f7705ec7711d984dc4a8fa54
Lili Dworkin <lilidworkin@gmail.com>
2017-07-14 08:26:29 -0600

    comments and linter

--- test/TestUtil.js
+++ test/TestUtil.js
@@ -10,7 +10,7 @@ const TestUtil = {};
 // Takes in an input string and returns a flattened and parsed node
 TestUtil.flattenAndParse = function (exprString) {
   return flatten(math.parse(exprString));
-}
+};
 
 // Tests a function that takes an input string and check its output
 TestUtil.testFunctionOutput = function (fn, input, output) {
