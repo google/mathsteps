6d8c6de79618372c78504e74b1da765c91149e04
Lili Dworkin <lilidworkin@gmail.com>
2017-07-14 10:20:46 -0600

    change groups and negatives

--- test/simplifyExpression/basicsSearch/mixedNumber.test.js
+++ test/simplifyExpression/basicsSearch/mixedNumber.test.js
@@ -7,6 +7,10 @@ function testIsMixedNumber(input, output) {
   TestUtil.testBooleanFunction(mixedNumber.isMixedNumber, input, output);
 }
 
+function testIsNegativeMixedNumber(input, output) {
+  TestUtil.testBooleanFunction(mixedNumber.isNegativeMixedNumber, input, output);
+}
+
 function testGetWholeNumber(input, output) {
   input = TestUtil.flattenAndParse(input);
   TestUtil.testFunctionOutput(mixedNumber.getWholeNumber, input, output);
@@ -30,6 +34,7 @@ describe('isMixedNumber', function () {
   const tests = [
     ['5(1)/(6)', true],
     ['19(2)/(3)', true],
+    ['-1(7)/(8)', true],
     ['4*(1/2)', false],
     ['(1/2)3', false],
     ['3*10/15', false],
@@ -37,10 +42,20 @@ describe('isMixedNumber', function () {
   tests.forEach(t => testIsMixedNumber(t[0], t[1]));
 });
 
+describe('isNegativeMixedNumber', function () {
+  const tests = [
+    ['-1(7)/(8)', true],
+    ['5(1)/(6)', false],
+    ['19(2)/(3)', false],
+  ];
+  tests.forEach(t => testIsNegativeMixedNumber(t[0], t[1]));
+});
+
 describe('getWholeNumber', function () {
   const tests = [
     ['5(1)/(6)', 5],
     ['19(2)/(3)', 19],
+    ['-1(7)/(8)', 1],
   ];
   tests.forEach(t => testGetWholeNumber(t[0], t[1]));
 });
@@ -49,6 +64,7 @@ describe('getNumerator', function () {
   const tests = [
     ['5(1)/(6)', 1],
     ['19(2)/(3)', 2],
+    ['-1(7)/(8)', 7],
   ];
   tests.forEach(t => testGetNumerator(t[0], t[1]));
 });
@@ -57,6 +73,7 @@ describe('getDenominator', function () {
   const tests = [
     ['5(1)/(6)', 6],
     ['19(2)/(3)', 3],
+    ['-1(7)/(8)', 8],
   ];
   tests.forEach(t => testGetDenominator(t[0], t[1]));
 });
@@ -75,11 +92,11 @@ describe('convertMixedNumberToImproperFraction', function() {
         '156/8'],
       '156/8'
     ],
-    ['5(10)/(11)',
-      ['((5 * 11) + 10) / 11',
-        '(55 + 10) / 11',
-        '65/11'],
-      '65/11'
+    ['-5(10)/(11)',
+      ['-((5 * 11) + 10) / 11',
+        '-(55 + 10) / 11',
+        '-65/11'],
+      '-65/11'
     ],
   ];
   tests.forEach(t => testConvertMixedNumberToImproperFraction(t[0], t[1], t[2]));
