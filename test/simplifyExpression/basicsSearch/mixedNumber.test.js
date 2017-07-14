8de537c3a4f6f3b54aa31d9a0e15866b62e7fe02
Lili Dworkin <lilidworkin@gmail.com>
2017-07-14 08:53:51 -0600

    adding more tests

--- test/simplifyExpression/basicsSearch/mixedNumber.test.js
+++ test/simplifyExpression/basicsSearch/mixedNumber.test.js
@@ -1,3 +1,5 @@
+const convertMixedNumberToImproperFraction = require(
+  '../../../lib/simplifyExpression/basicsSearch/convertMixedNumberToImproperFraction');
 const mixedNumber = require('../../../lib/simplifyExpression/basicsSearch/mixedNumber');
 const TestUtil = require('../../TestUtil');
 
@@ -18,6 +20,10 @@ function testGetNumerator(input, output) {
 function testGetDenominator(input, output) {
   input = TestUtil.flattenAndParse(input);
   TestUtil.testFunctionOutput(mixedNumber.getDenominator, input, output);
+}
+
+function testConvertMixedNumberToImproperFraction(exprString, outputList, outputStr) {
+  TestUtil.testSubsteps(convertMixedNumberToImproperFraction, exprString, outputList, outputStr);
 }
 
 describe('isMixedNumber', function () {
@@ -53,4 +59,28 @@ describe('getDenominator', function () {
     ['19(2)/(3)', 3],
   ];
   tests.forEach(t => testGetDenominator(t[0], t[1]));
+});
+
+describe('convertMixedNumberToImproperFraction', function() {
+  const tests = [
+    ['1(2)/(3)',
+      ['((1 * 3) + 2) / 3',
+        '(3 + 2) / 3',
+        '5/3'],
+      '5/3'
+    ],
+    ['19(4)/(8)',
+      ['((19 * 8) + 4) / 8',
+        '(152 + 4) / 8',
+        '156/8'],
+      '156/8'
+    ],
+    ['5(10)/(11)',
+      ['((5 * 11) + 10) / 11',
+        '(55 + 10) / 11',
+        '65/11'],
+      '65/11'
+    ],
+  ];
+  tests.forEach(t => testConvertMixedNumberToImproperFraction(t[0], t[1], t[2]));
 });
