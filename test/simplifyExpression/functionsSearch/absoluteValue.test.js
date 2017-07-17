d274731fc0284c9fd0f60311dbfa13be2cbc9013
Lili Dworkin <lilidworkin@gmail.com>
2017-07-17 14:45:42 -0400

    absolute value of negative numbers

--- test/simplifyExpression/functionsSearch/absoluteValue.test.js
+++ test/simplifyExpression/functionsSearch/absoluteValue.test.js
@@ -10,6 +10,9 @@ describe('abs', function () {
   const tests = [
     ['abs(4)', '4'],
     ['abs(-5)', '5'],
+    ['abs(-x)', 'x'],
+    ['abs(-xy)', 'xy'],
+    ['abs(-x/2)', '1/2 x'],
   ];
   tests.forEach(t => testAbsoluteValue(t[0], t[1]));
 });
