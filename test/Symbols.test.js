16db38aca092d032f1cddecbff444c361ec3c690
Richard Lee <rblee88@gmail.com>
2017-07-18 23:22:24 -0700

    Recursively check symbol nodes until you have a term without addition
    The parsed nodes were previously comparing something like:
    
    2 + 2/x + x/2
    
    as 2 nodes.  One node of `2 + 2/x` being added to `x/2`
    
    We needed to drill down to check `2` and `2/x` and `x/2` as individual
    nodes

--- test/Symbols.test.js
+++ test/Symbols.test.js
@@ -4,10 +4,10 @@ const math = require('mathjs');
 const print = require('../lib/util/print');
 const Symbols = require('../lib/Symbols');
 
-function runTest(functionToTest, exprString, expectedOutput) {
+function runTest(functionToTest, exprString, expectedOutput, symbolName) {
   it(exprString + ' -> ' + expectedOutput, function () {
     const expression = math.parse(exprString);
-    const foundSymbol = functionToTest(expression, 'x');
+    const foundSymbol = functionToTest(expression, symbolName);
     assert.deepEqual(
       print(foundSymbol),
       expectedOutput
@@ -17,26 +17,43 @@ function runTest(functionToTest, exprString, expectedOutput) {
 
 describe('getLastSymbolTerm', function() {
   const tests = [
-    ['1/x', '1 / x'],
-    ['1/(3x)', '1 / (3x)'],
-    ['3x', '3x'],
-    ['x + 3x', '3x'],
-    ['x/(x+3)', 'x / (x + 3)'],
+    ['1/x', '1 / x', 'x'],
+    ['1/(3x)', '1 / (3x)', 'x'],
+    ['3x', '3x', 'x'],
+    ['x + 3x + 2', '3x', 'x'],
+    ['x/(x+3)', 'x / (x + 3)', 'x'],
+    ['x/(x+3) + y', 'x / (x + 3)', 'x'],
+    ['x/(x+3) + y + 3x', 'y', 'y'],
+    ['x/(x+3) + y + 3x + 1/2y', '1/2 y', 'y'],
   ];
 
-  tests.forEach(t => runTest(Symbols.getLastSymbolTerm, t[0], t[1]));
+  tests.forEach(t => runTest(Symbols.getLastSymbolTerm, t[0], t[1], t[2]));
+});
+
+describe('getLastNonSymbolTerm', function() {
+  const tests = [
+    ['4x^2 + 2x + 2/4', '2/4', 'x'],
+    ['4x^2 + 2/4 + x', '2/4', 'x'],
+    ['4x^2 + 2x + y', 'y', 'x'],
+    ['4x^2', '4', 'x'],
+  ];
+
+  tests.forEach(t => runTest(Symbols.getLastNonSymbolTerm, t[0], t[1], t[2]));
 });
 
 describe('getLastDenominatorWithSymbolTerm', function() {
   const tests = [
-    ['1/x', 'x'],
-    ['1/(x+2)', '(x + 2)'],
-    ['1/(x+2) + 3x', '(x + 2)'],
-    ['1/(x+2) + 3x/(1+x)', '(1 + x)'],
-    ['1/(x+2) + (x+1)/(2x+3)', '(2x + 3)'],
-    ['1/x + x/5', 'x'],
+    ['1/x', 'x', 'x'],
+    ['1/(x+2)', '(x + 2)', 'x'],
+    ['1/(x+2) + 3x', '(x + 2)', 'x'],
+    ['1/(x+2) + 3x/(1+x)', '(1 + x)', 'x'],
+    ['1/(x+2) + (x+1)/(2x+3)', '(2x + 3)', 'x'],
+    ['1/x + x/5', 'x', 'x'],
+    ['2 + 2/x + x/2', 'x', 'x'],
+    ['2 + 2/y + x/2', 'y', 'y'],
+    ['2y + 2/x + 3/(2y) + x/2', '(2y)', 'y'],
   ];
 
-  tests.forEach(t => runTest(Symbols.getLastDenominatorWithSymbolTerm, t[0], t[1]));
+  tests.forEach(t => runTest(Symbols.getLastDenominatorWithSymbolTerm, t[0], t[1], t[2]));
 });
 
