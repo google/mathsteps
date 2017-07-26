4849ef896c7711fc1f5439618de8237fc00f665a
SirFurness <eric.1334@icloud.com>
2017-07-25 23:41:30 -0700

    Add LaTeX printing
    Prints out equation as LaTeX. It removes +- based on the showPlusMinus
    variable, and it changes explicit multiplication of the type 2*x to
    2x. Commit attempts resolve #160

--- test/equation.test.js
+++ test/equation.test.js
@@ -10,6 +10,21 @@ function constructAndPrintEquation(left, right, comp) {
   return equation.print();
 }
 
+function constructAndPrintLatexEquation(left, right, comp) {
+  const leftNode = math.parse(left);
+  const rightNode = math.parse(right);
+  const equation = new Equation(leftNode, rightNode, comp);
+  return equation.printLatex();
+}
+
+function testLatexPrint(left, right, comp, output) {
+  it (output, () => {
+    assert.equal(
+      constructAndPrintLatexEquation(left, right, comp), output
+    );
+  });
+}
+
 function testEquationConstructor(left, right, comp, output) {
   it (output, () => {
     assert.equal(
@@ -25,4 +40,13 @@ describe('Equation constructor', () => {
     ['2*x - 1', '0', '<=', '2x - 1 <= 0']
   ];
   tests.forEach(t => testEquationConstructor(t[0], t[1], t[2], t[3]));
+});
+
+describe('Latex printer', () => {
+  const tests = [
+    ['2*x^2 + x', '4', '=', '2{x}^{2}+ x = 4'],
+    ['x^2 + 2*x + 2', '0', '>=', '{ x}^{2}+2x+2 >= 0'],
+    ['2*x + -1', '0', '<=', '2x - 1 <= 0']
+  ];
+  tests.forEach(t => testLatexPrint(t[0], t[1], t[2], t[3]));
 });
