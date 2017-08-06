221a5639abb9e39c316f7b7f6589944127310488
evykassirer <evy.kassirer@gmail.com>
2017-08-05 22:47:14 -0700

    fixed all the op stuff

--- test/solveEquation/solveEquation.test.js
+++ test/solveEquation/solveEquation.test.js
@@ -125,6 +125,7 @@ describe('solveEquation for non = comparators', function() {
 
 function testSolveConstantEquation(equationString, expectedChange, debug=false) {
   const steps = solveEquation(equationString, debug);
+  console.log(steps);
   const actualChange = steps[steps.length -1].changeType;
   it(equationString + ' -> ' + expectedChange, (done) => {
     assert.equal(actualChange, expectedChange);
