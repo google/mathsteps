1428f4df70a4fc3c5921a31d5fbbaa8bd8bf61fb
evykassirer <evy.kassirer@gmail.com>
2017-08-13 14:44:02 -0700

    updated distribution now that paren nodes arent explicitly parsed

--- test/simplifyExpression/distributeSearch/distributeSearch.test.js
+++ test/simplifyExpression/distributeSearch/distributeSearch.test.js
@@ -8,18 +8,18 @@ function testDistribute(exprStr, outputStr) {
 
 describe('distribute - into paren with addition', function () {
   const tests = [
-    ['-(x+3)', '(-x - 3)'],
-    ['-(x - 3)', '(-x + 3)'],
-    ['-(-x^2 + 3y^6)' , '(x^2 - 3y^6)'],
+    ['-(x+3)', '-x - 3'],
+    ['-(x - 3)', '-x + 3'],
+    ['-(-x^2 + 3y^6)' , 'x^2 - 3 y^6'], // TODO(printing) spaces with coefficients
   ];
   tests.forEach(t => testDistribute(t[0], t[1]));
 });
 
 describe('distribute - into paren with multiplication/division', function () {
   const tests = [
-    ['-(x*3)', '(-x * 3)'],
-    ['-(-x * 3)', '(x * 3)'],
-    ['-(-x^2 * 3y^6)', '(x^2 * 3y^6)'],
+    ['-(x*3)', '-x * 3'],
+    ['-(-x * 3)', 'x * 3'],
+    ['-(-x^2 * 3y^6)', 'x^2 * 3 y^6'], // TODO(printing) spaces with coefficients
   ];
   tests.forEach(t => testDistribute(t[0], t[1]));
 });
@@ -32,20 +32,20 @@ function testDistributeSteps(exprString, outputList) {
 describe('distribute', function () {
   const tests = [
     ['x*(x+2+y)',
-      ['(x * x + x * 2 + x * y)',
-        '(x^2 + 2x + x * y)']
+      ['x * x + x * 2 + x * y',
+        'x^2 + 2x + x * y']
     ],
     ['(x+2+y)*x*7',
-      ['(x * x + 2x + y * x) * 7',
-        '(x^2 + 2x + y * x) * 7']
+      ['(x * x + 2 * x + y * x) * 7',
+        '(x^2 + 2 * x + y * x) * 7']
     ],
     ['(5+x)*(x+3)',
-      ['(5 * (x + 3) + x * (x + 3))',
-        '((5x + 15) + (x^2 + 3x))']
+      ['5 * (x + 3) + x * (x + 3)',
+        '(5 * x + 15) + (x^2 + 3x)']
     ],
     ['-2x^2 * (3x - 4)',
-      ['(-2x^2 * 3x - 2x^2 * -4)',
-        '(-6x^3 + 8x^2)']
+      ['-2 x^2 * 3x - 2 x^2 * -4', // TODO(printing) spaces with coefficients
+        '-6 x^3 + 8 x^2'] // TODO(printing) spaces with coefficients
     ],
   ];
   tests.forEach(t => testDistributeSteps(t[0], t[1]));
@@ -55,30 +55,30 @@ describe('distribute with fractions', function () {
   const tests = [
     // distribute the non-fraction term into the numerator(s)
     ['(3 / x^2 + x / (x^2 + 3)) * (x^2 + 3)',
-      '((3 * (x^2 + 3)) / (x^2) + (x * (x^2 + 3)) / (x^2 + 3))',
+      '(3 * (x^2 + 3)) / x^2 + (x * (x^2 + 3)) / (x^2 + 3)',
     ],
 
     // if both groupings have fraction, the rule does not apply
     ['(3 / x^2 + x / (x^2 + 3)) * (5 / x + x^5)',
-      '((3 / (x^2) * 5 / x + 3 / (x^2) * x^5) + (x / (x^2 + 3) * 5 / x + x / (x^2 + 3) * x^5))',
+      '(3 / x^2 * 5/x + 3 / x^2 * x^5) + (x / (x^2 + 3) * 5/x + x / (x^2 + 3) * x^5)',
     ],
   ];
 
   const multiStepTests = [
 
     ['(2 / x +  3x^2) * (x^3 + 1)',
-      ['((2 * (x^3 + 1)) / x + 3x^2 * (x^3 + 1))',
-        '((2 * (x^3 + 1)) / x + (3x^5 + 3x^2))']
+      ['(2 * (x^3 + 1)) / x + 3 x^2 * (x^3 + 1)', // TODO(printing) spaces with coefficients
+        '(2 * (x^3 + 1)) / x + (3 x^5 + 3 x^2)'] // TODO(printing) spaces with coefficients
     ],
 
     ['(2x + x^2) * (1 / (x^2 -4) + 4x^2)',
-      ['((1 * (2x + x^2)) / (x^2 - 4) + 4x^2 * (2x + x^2))',
-        '((1 * (2x + x^2)) / (x^2 - 4) + (8x^3 + 4x^4))']
+      ['(1 * (2x + x^2)) / (x^2 - 4) + 4 x^2 * (2x + x^2)', // TODO(printing) spaces with coefficients
+        '(1 * (2x + x^2)) / (x^2 - 4) + (8 x^3 + 4 x^4)'] // TODO(printing) spaces with coefficients
     ],
 
-    ['(2x + x^2) * (3x^2 / (x^2 -4) + 4x^2)',
-      ['((3x^2 * (2x + x^2)) / (x^2 - 4) + 4x^2 * (2x + x^2))',
-        '((3x^2 * (2x + x^2)) / (x^2 - 4) + (8x^3 + 4x^4))']
+    ['(2x + x^2) * ((3x^2) / (x^2 -4) + 4x^2)',
+      ['(3 x^2 * (2x + x^2)) / (x^2 - 4) + 4 x^2 * (2x + x^2)', // TODO(printing) spaces with coefficients
+        '(3 x^2 * (2x + x^2)) / (x^2 - 4) + (8 x^3 + 4 x^4)']  // TODO(printing) spaces with coefficients
     ],
 
   ];
@@ -90,9 +90,9 @@ describe('distribute with fractions', function () {
 
 describe('expand base', function () {
   const tests = [
-    ['(nthRoot(x, 2))^2','nthRoot(x, 2) * nthRoot(x, 2)'],
-    ['(nthRoot(x, 2))^3','nthRoot(x, 2) * nthRoot(x, 2) * nthRoot(x, 2)'],
-    ['3 * (nthRoot(x, 2))^4', '3 * nthRoot(x, 2) * nthRoot(x, 2) * nthRoot(x, 2) * nthRoot(x, 2)'],
+    ['(nthRoot(x, 2))^2', 'nthRoot(x, 2) * nthRoot(x, 2)'],
+    ['(nthRoot(x, 2))^3', 'nthRoot(x, 2) * nthRoot(x, 2) * nthRoot(x, 2)'],
+    ['3 * nthRoot(x, 2)^4', '3 * (nthRoot(x, 2) * nthRoot(x, 2) * nthRoot(x, 2) * nthRoot(x, 2))'],
     ['(nthRoot(x, 2) + nthRoot(x, 3))^2', '(nthRoot(x, 2) + nthRoot(x, 3)) * (nthRoot(x, 2) + nthRoot(x, 3))'],
     ['(2x + 3)^2', '(2x + 3) * (2x + 3)'],
     ['(x + 3 + 4)^2', '(x + 3 + 4) * (x + 3 + 4)'],
