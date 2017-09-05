e1975617170ea39775d1d3af021f785e0ce6fe1b
evykassirer <evy.kassirer@gmail.com>
2017-09-04 23:38:52 -0400

    fix some cancelling tests

--- test/simplifyExpression/fractionsSearch/cancelLikeTerms.test.js
+++ test/simplifyExpression/fractionsSearch/cancelLikeTerms.test.js
@@ -8,14 +8,16 @@ function testCancelLikeTerms(exprStr, expectedStr) {
 
 describe('cancel like terms', function () {
   const tests = [
-//    ['2/2', '1'],
-//    ['x^2/x^2', '1'],
-//    ['x^3/x^2', 'x^(3 - (2))'], // parens will be removed at end of step
+    ['2/2', '1'],
+    ['x^2/x^2', '1'],
+    ['x^3/x^2', 'x^(3 - (2))'], // parens will be removed at end of step
     ['(x^3*y)/x^2', 'x^(3 - (2)) * y'],
-/*
     ['-(7+x)^8/(7+x)^2', '-(7 + x)^(8 - (2))'],
-    ['(2x^2 * 5) / (2x^2)', '5'], // these parens have to stay around 2x^2 to be parsed correctly.
-    ['(x^2 * y) / x', '(x^(2 - (1)) * y)'],
+    // TODO: this isn't cancelling because 2x^2 != 2*x^2
+    // which is weird cause idk where the * is coming from (maybe flatten?)
+    // but also cancelling should be updated to deal with that case
+    ['(2x^2 * 5) / (2x^2)', '5'], // these parens have to stay around 2x^2 to be parsed correctly - hopefully the parsing coefficient change fixes that
+    ['(x^2 * y) / x', 'x^(2 - (1)) * y'],
     ['2x^2 / (2x^2 * 5)', '1/5'],
     ['x / (x^2*y)', 'x^(1 - (2)) / y'],
     ['(4x^2) / (5x^2)', '4/5'],
@@ -24,10 +26,10 @@ describe('cancel like terms', function () {
     ['-x / -x', '1'],
     ['2/ (4x)', '1 / (2x)'],
     ['2/ (4x^2)', '1 / (2 x^2)'], // TODO(printing) no spaces for coefficients
-    ['2 a / a', '2'],
-    ['(35 * nthRoot (7)) / (5 * nthRoot(5, 2))','(7 * nthRoot(7, 2)) / nthRoot(5)'],
+    ['(2a) / a', '2'],
+    ['(35 * nthRoot (7)) / (5 * nthRoot(5, 2))','(7 * nthRoot(7, 2)) / nthRoot(5, 2)'],
     ['3/(9r^2)', '1 / (3 r^2)'], // TODO(printing) no spaces for coefficients
-    ['6/(2x)', '3/x']*/
+    ['6/(2x)', '3/x']
   ];
 
   tests.forEach(t => testCancelLikeTerms(t[0], t[1]));
