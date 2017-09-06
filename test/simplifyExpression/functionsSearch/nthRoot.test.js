79c8533762e58d85f0239ae8f3ed7613171cf506
evykassirer <evy.kassirer@gmail.com>
2017-09-05 22:12:03 -0400

    fix remaining nthroot tests

--- test/simplifyExpression/functionsSearch/nthRoot.test.js
+++ test/simplifyExpression/functionsSearch/nthRoot.test.js
@@ -14,7 +14,7 @@ describe('simplify nthRoot', function () {
     ['nthRoot(4, 3)', 'nthRoot(4, 3)'],
     ['nthRoot(12)', '2 * nthRoot(3, 2)'],
     ['nthRoot(36)', '6'],
-    ['nthRoot(72)', '2 * 3 * nthRoot(2, 2)'],
+    ['nthRoot(72)', '2 * nthRoot(2, 2) * 3'],
     ['nthRoot(x^2)', 'x'],
     ['nthRoot(x^3)', 'nthRoot(x^3, 2)'],
     ['nthRoot(x^3, 3)', 'x'],
@@ -39,25 +39,24 @@ function testNthRootSteps(exprString, outputList) {
 describe('nthRoot steps', function () {
   const tests = [
     ['nthRoot(12)',
-      ['nthRoot(2 * 2 * 3)',
+      ['nthRoot(2 * 2 * 3, 2)',
         'nthRoot((2 * 2) * 3, 2)',
-        'nthRoot(2 ^ 2 * 3, 2)',
-        'nthRoot(2 ^ 2, 2) * nthRoot(3, 2)',
+        'nthRoot(2^2 * 3, 2)',
+        'nthRoot(2^2, 2) * nthRoot(3, 2)',
         '2 * nthRoot(3, 2)']
     ],
     ['nthRoot(72)',
-      ['nthRoot(2 * 2 * 2 * 3 * 3)',
+      ['nthRoot(2 * 2 * 2 * 3 * 3, 2)',
         'nthRoot((2 * 2) * 2 * (3 * 3), 2)',
-        'nthRoot(2^2 * 2 * 3 ^ 2, 2)',
+        'nthRoot(2^2 * 2 * 3^2, 2)',
         'nthRoot(2^2, 2) * nthRoot(2, 2) * nthRoot(3^2, 2)',
-        '2 * nthRoot(2, 2) * 3',
-        '2 * 3 * nthRoot(2, 2)']
+        '2 * nthRoot(2, 2) * 3']
     ],
     ['nthRoot(36*x)',
-      ['nthRoot(2 * 2 * 3 * 3 * x)',
+      ['nthRoot(2 * 2 * 3 * 3 * x, 2)',
         'nthRoot((2 * 2) * (3 * 3) * x, 2)',
-        'nthRoot(2 ^ 2 * 3^2 * x, 2)',
-        'nthRoot(2 ^ 2, 2) * nthRoot(3^2, 2) * nthRoot(x, 2)',
+        'nthRoot(2^2 * 3^2 * x, 2)',
+        'nthRoot(2^2, 2) * nthRoot(3^2, 2) * nthRoot(x, 2)',
         '2 * 3 * nthRoot(x, 2)']
     ],
     ['nthRoot(2 * 18 * x^2, 2)',
@@ -70,7 +69,7 @@ describe('nthRoot steps', function () {
     ['nthRoot(32, 3)',
       ['nthRoot(2 * 2 * 2 * 2 * 2, 3)',
         'nthRoot((2 * 2 * 2) * (2 * 2), 3)',
-        'nthRoot(2^3 * 2 ^ 2, 3)',
+        'nthRoot(2^3 * 2^2, 3)',
         'nthRoot(2^3, 3) * nthRoot(2^2, 3)',
         '2 * nthRoot(2^2, 3)']
     ],
