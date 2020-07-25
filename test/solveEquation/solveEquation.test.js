33487b7d5fe414f0d9879fd19f87ff6f7370086d
Sylwester Wysocki <dzik@ke.mu>
2020-07-25 22:17:43 +0200

    Tests/SolveEquation: Imported cases from GabrielMahan/mathsteps..

--- test/solveEquation/solveEquation.test.js
+++ test/solveEquation/solveEquation.test.js
@@ -102,8 +102,20 @@ describe('solveEquation for =', function () {
     ['2/(1 + 1 + 4x) = 1/3', 'x = 1'],
     ['(3 + x) / (x^2 + 3) = 1', 'x = [0, 1]'],
     ['6/x + 8/(2x) = 10', 'x = 1'],
+
+    // Imported from https://github.com/florisdf/mathsteps/blob/master/test/solveEquation/solveEquation.test.js
     ['(x+1)=4', 'x = 3'],
     ['((x)/(4))=4', 'x = 16'],
+
+    // Imported from: https://github.com/GabrielMahan/mathsteps/blob/master/test/solveEquation/solveEquation.test.js
+    ['(2x-12)=(x+4)', 'x = 16'],
+    ['x+y=x+y', '0 = 0'],
+    ['y + 2x = 14 + y', 'x = 7'],
+    ['((1)/(2+1)) = ((1)/(3))', '1/3 = 1/3'],
+    ['-((1)/(3)) = ((-1)/(3))', '-(1/3) = -1/3'],
+    ['-(x/2)=3', 'x = -6'],
+    ['44x=2.74', 'x = 137/2200'],
+
     // TODO: fix these cases, fail because lack of factoring support, for complex #s,
     // for taking the sqrt of both sides, etc
     // ['(x + y) (y + 2) = 0', 'y = -y'],
