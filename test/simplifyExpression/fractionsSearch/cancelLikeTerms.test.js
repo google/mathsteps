16af45e8e159b2821a94dda49641a0c6f8c522a0
Sylwester Wysocki <dzik@ke.mu>
2020-07-27 06:09:41 +0200

    Tests/FractionSearch: Imported cases from al_stepper_stuck.

--- test/simplifyExpression/fractionsSearch/cancelLikeTerms.test.js
+++ test/simplifyExpression/fractionsSearch/cancelLikeTerms.test.js
@@ -10,6 +10,23 @@ describe('cancel like terms', function () {
   const tests = [
     ['2/2', '1'],
     ['x^2/x^2', '1'],
+
+    // -------------------------------------------------------------------------
+    // Cases from al_stepper_stuck
+    ['x^3/x^2', 'x'], // parens will be removed at end of step
+    ['(x^3*y)/x^2', 'x * y'],
+    //['-(7+x)^8/(7+x)^2', '-(7 + x)^6'],
+    //['(2x^2 * 5) / (2x^2)', '5'], // these parens have to stay around 2x^2 to be parsed correctly.
+    ['(x^2 * y) / x', 'x * y'],
+    //['2x^2 / (2x^2 * 5)', '1/5'],
+    //['x / (x^2*y)', 'x^(1 - (2)) / y'],
+    ['(4x^2) / (5x^2)', '4/5'],
+    ['(2x+5)^8 / (2x+5)^2', '(2x + 5)^6'],
+    ['(4x^3) / (5x^2)', '4x / 5'],
+    // -------------------------------------------------------------------------
+
+    // TODO: Review it.
+    // TODO: These tests are overwriten above.
     ['x^3/x^2', 'x^(3 - (2))'], // parens will be removed at end of step
     ['(x^3*y)/x^2', '(x^(3 - (2)) * y)'],
     ['-(7+x)^8/(7+x)^2', '-(7 + x)^(8 - (2))'],
@@ -22,6 +39,7 @@ describe('cancel like terms', function () {
     ['(4x^3) / (5x^2)', '(4x^(3 - (2))) / (5)'],
     ['-x / -x', '1'],
     ['2/ (4x)', '1 / (2x)'],
+
     ['2/ (4x^2)', '1 / (2x^2)'],
     ['2 a / a', '2'],
     ['(35 * nthRoot (7)) / (5 * nthRoot(5))','(7 * nthRoot(7)) / nthRoot(5)'],
