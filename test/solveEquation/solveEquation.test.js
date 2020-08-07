eaf58f46b537855a3f9cea840ce677a5009f114b
Sylwester Wysocki <dzik@ke.mu>
2020-08-07 23:55:03 +0200

    Tests/SolveEquation: Enabled temporary disabled tests.

--- test/solveEquation/solveEquation.test.js
+++ test/solveEquation/solveEquation.test.js
@@ -53,21 +53,17 @@ describe('solveEquation for =', function () {
     // TODO: They're uncommented on al_foil.
     // 2 test cases from al_distribute_over_mult
     ['(2x^2 - 1)(x^2 - 5)(x^2 + 5) = 0', 'x = [-1 / sqrt(2), 1 / sqrt(2), -nthRoot(5, 2), nthRoot(5, 2)]'],
-    // TODO: Crash: ['(-x ^ 2 - 4x + 2)(-3x^2 - 6x + 3) = 0', '3x^4 + 18x^3 + 15x^2 - 24x = -6'],
+    // TODO: Partial result: ['(-x ^ 2 - 4x + 2)(-3x^2 - 6x + 3) = 0', '3x^4 + 18x^3 + 15x^2 - 24x = -6'],
 
     ['5x + (1/2)x = 27 ', 'x = 54/11'],
-    /* Temporary disabled due to breaking changes in mathjs.
-       negate(2x/3) gives -2/3x when mathjs@4.0.0 or later is used.
-
     ['2x/3 = 2x - 4 ', 'x = 3'],
     ['(-2/3)x + 3/7 = 1/2', 'x = -3/28'],
-    */
     ['(-9/4)v + 4/5 = 7/8', 'v = -1/30'],
     ['x^2 - 2 = 0', 'x = [-nthRoot(2, 2), nthRoot(2, 2)]'],
     ['x/(2/3) = 1', 'x = 2/3'],
     ['(x+1)/3 = 4', 'x = 11'],
     ['2(x+3)/3 = 2', 'x = 0'],
-    // TODO: Bad result: ['( u )/( 0.3) = 4u + 6.28', 'u = -9.42'],
+    ['( u )/( 0.3) = 4u + 6.28', 'u = -471/50'],
 
     ['- q - 4.36= ( 2.2q )/( 1.8)', 'q = -981/500'],
     ['5x^2 - 5x - 30 = 0', 'x = [-2, 3]'],
@@ -111,7 +107,7 @@ describe('solveEquation for =', function () {
     // Nth root support
     // TODO: + or - for sqrt roots
     ['x^2 - 2 = 0', 'x = [-nthRoot(2, 2), nthRoot(2, 2)]'],
-    // TODO: Crash: ['x^2 = 1', 'x = [-1, 1]'],
+    // TODO: No steps: ['x^2 = 1', 'x = [-1, 1]'],
     ['x^3 - 3 = 0', 'x = [nthRoot(3, 3), nthRoot(3, 3), nthRoot(3, 3)]'],
     ['(x^2 - 2) (x^2 - 5) = 0', 'x = [-nthRoot(2, 2), nthRoot(2, 2), -nthRoot(5, 2), nthRoot(5, 2)]'],
     ['(x^2 + 2) (x^3 - 7) = 0', 'x = [nthRoot(7, 3), nthRoot(7, 3), nthRoot(7, 3)]'],
@@ -119,8 +115,8 @@ describe('solveEquation for =', function () {
     // TODO: Partial result: ['(y + 1)^2 = 1', 'y = 0'],
     // TODO: Partial result: ['(y + 1)^3 = 8', 'y = 1'],
     // TODO: Partial result: ['(2x + 1)^3 = 1', 'x = 0'],
-    // TODO: Crash: ['(3x + 2)^2 = 2', 'x = nthRoot(2, 2) / 3 - 2/3'],
-    // TODO: Crash: ['(3x + 2)^2 + 2 = 1']
+    // TODO: Partial result: ['(3x + 2)^2 = 2', 'x = nthRoot(2, 2) / 3 - 2/3'],
+    // TODO: Partial result: ['(3x + 2)^2 + 2 = 1']
     // -------------------------------------------------------------------------
 
     // 1 test case from al_distribute_over_mult
@@ -152,13 +148,13 @@ describe('solveEquation for =', function () {
     ['((x-2)^2) = 0', 'x = [2, 2]'],
     // TODO: x vs y: ['(x + y) (y + 2) = 0', 'x = [-y, -2]'],
     // TODO: No steps: ['x * x (x - 5)^2 = 0', NO_STEPS],
-    // TODO: No steps: ['x^6 - x', NO_STEPS],
+    // TODO: Partial result: ['x^6 - x = 0', NO_STEPS],
 
-    // TODO: No steps: ['4x^2 - 25y^2', ''],
+    // TODO: Partial result: ['4x^2 - 25y^2', ''],
     ['(x^2 + 2x + 1) (x^2 + 3x + 2) = 0', 'x = [-1, -1, -1, -2]'],
     ['(2x^2 - 1)(x^2 - 5)(x^2 + 5) = 0', 'x = [-1 / sqrt(2), 1 / sqrt(2), -nthRoot(5, 2), nthRoot(5, 2)]'],
-    // TODO: Crash: ['(-x ^ 2 - 4x + 2)(-3x^2 - 6x + 3) = 0', ''],
-    // TODO: Crash: ['x^2 = -2x - 1', 'x = -1'],
+    // TODO: Too many steps: ['(-x ^ 2 - 4x + 2)(-3x^2 - 6x + 3) = 0', ''],
+    ['x^2 = -2x - 1', 'x = [-1, -1]'],
     ['x - 3.4= ( x - 2.5)/( 1.3)', 'x = 32/5']
 
   ]
